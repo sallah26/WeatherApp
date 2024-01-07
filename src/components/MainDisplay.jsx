b2316835aa42ef47e49e5f3c1d20b7baa51233ff
sallah26 <143meski@gmail.com>
2024-01-07 21:53:56 +0300

    displaying cards are responsed

--- src/components/MainDisplay.jsx
+++ src/components/MainDisplay.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const MainDisplay = ({ weatherData, city }) => {
   return (
-    <div className="lg:absolute">
+    <div className="">
       {weatherData && (
-        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          <div className="first">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-1">
+          <div className="first min-w-52">
             <p>Pressure: {weatherData.main.pressure}</p>
             <p>Humidity: {weatherData.main.humidity}%</p>
             <p>Max-Temperature: {weatherData.main.temp_max}°C</p>
             <p>Min-Temperature: {weatherData.main.temp_min}°C</p>
           </div>
-          <div className="second">
+          <div className="second col-start-2 col-span-4 text-center">
             <p className="text-6xl font-bold">{weatherData.name}</p>
 
             <p>Temperature: {weatherData.main.temp}°C</p>
