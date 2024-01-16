1513d8741bce4fca2faef7bda2fd4d97cd5dca49
Selahadin Hamid <120811433+sallah26@users.noreply.github.com>
2024-01-16 10:47:56 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ import FavouriteCities from "./components/FavouriteCities";
 //     try {
 //     try {
 //     try {
+//     try {
+//     try {
 //       const response = await fetch(
 //         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 //       );ose custome directionsappend sa
